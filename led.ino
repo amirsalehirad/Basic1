6535eaa6c6be36090267667d6b162d9ceb9c44d0
amirsalehirad <138507857+amirsalehirad@users.noreply.github.com>
2023-12-21 18:41:25 +0330

    Update led.ino
    turning an LED on and off

--- led.ino
+++ led.ino
@@ -1,12 +1,13 @@
 void setup() { // initialize digital pin 13 as an output
   // put your setup code here, to run once:
-pinMode(2, OUTPUT);
+pinMode(13, OUTPUT);
 }
 
+  byte red=13;
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(2, HIGH); // turn the LED on (HIGH is the voltage level)
+  digitalWrite(13, HIGH); // turn the LED on (HIGH is the voltage level)
   delay(1000); // wait for a second
-  digitalWrite(2, LOW); // turn the LED off by making the voltage LOW
+  digitalWrite(13, LOW); // turn the LED off by making the voltage LOW
   delay(1000); // wait for a second
 }
