3c5a614f449e827f601b6070ab8869870363af11
amirsalehirad <138507857+amirsalehirad@users.noreply.github.com>
2023-12-23 13:58:02 +0330

    Update simple_traffic_light.ino

--- simple_traffic_light.ino
+++ simple_traffic_light.ino
@@ -1,4 +1,4 @@
-void setup() { // initialize digital pin 13 as an output
+void setup() { 
   // put your setup code here, to run once:
 pinMode(13, OUTPUT);
 pinMode(7, OUTPUT);
@@ -19,8 +19,8 @@ void loop() {
   digitalWrite(red, LOW); // turn the LED off by making the voltage LOW
   digitalWrite(green, LOW); // turn the LED off by making the voltage LOW
   delay(1000); // wait for a second
-  digitalWrite(green, HIGH); // turn the LED off by making the voltage LOW
+  digitalWrite(green, HIGH); // turn the LED on (HIGH is the voltage level)
   digitalWrite(red, LOW); // turn the LED off by making the voltage LOW
   digitalWrite(yellow, LOW); // turn the LED off by making the voltage LOW
-  delay(1000);
+  delay(1000); // wait for a second
 }
